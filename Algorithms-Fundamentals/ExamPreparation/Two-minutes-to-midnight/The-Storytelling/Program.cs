acb69c358283d5e8bb0a5edb164c280796f387b8
DenisK111 <denislavkraev@gmail.com>
2022-07-19 12:52:57 +0300

    Added Exam-Preparation

---
+++ Algorithms-Fundamentals/ExamPreparation/Two-minutes-to-midnight/The-Storytelling/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static Dictionary<string, List<string>> graph;
+        static HashSet<string> visited;
+        static LinkedList<string> sorted;
+        static HashSet<string> cycles;
+        static void Main(string[] args)
+        {
+
+            sorted = new LinkedList<string>();
+            graph = new Dictionary<string, List<string>>();
+            visited = new HashSet<string>();
+            cycles = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split("->", StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "End")
+                {
+                    break;
+                }
+                if (input.Length > 1)
+                {
+                    graph[input[0].Trim()] = new List<string>(input[1].Trim().Split());
+                }
+
+                else
+                {
+                    graph[input[0].Trim()] = new List<string>();
+                }
+            }
+            var isCyclic = false;
+            foreach (var node in graph.Keys)
+            {
+                try
+                {
+                    DFS(node);
+
+                }
+                catch (Exception ex)
+                {
+                    isCyclic = true;
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+
+
+            if (!isCyclic)
+            {
+
+                Console.WriteLine(String.Join(" ", sorted));
+            }
+        }
+
+        private static void DFS(string node)
+        {
+            if (cycles.Contains(node))
+            {
+                throw new InvalidOperationException("Invalid topological sorting");
+            }
+
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            cycles.Add(node);
+            visited.Add(node);
+
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+            sorted.AddFirst(node);
+            cycles.Remove(node);
+        }
+    }
+}
---
+++ Algorithms-Fundamentals/ExamPreparation/Two-minutes-to-midnight/Time/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var array2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var lcs = new int[array1.Length + 1, array2.Length + 1];
+
+            for (int r = 1; r < lcs.GetLength(0); r++)
+            {
+                for (int c = 1; c < lcs.GetLength(1); c++)
+                {
+                    if (array1[r - 1] == array2[c - 1])
+                    {
+                        lcs[r, c] = lcs[r - 1, c - 1] + 1;
+                    }
+
+                    else
+                    {
+                        lcs[r, c] = Math.Max(lcs[r, c - 1], lcs[r - 1, c]);
+                    }
+                }
+            }
+
+            /// RECOVER THE LCS
+            var sequence = new List<int>();
+            var row = array1.Length;
+            var col = array2.Length;
+            while (row > 0 && col > 0)
+            {
+                if (array1[row - 1] == array2[col - 1] &&
+                    lcs[row, col] == lcs[row - 1, col - 1] + 1)
+                {
+                    sequence.Add(array1[row - 1]);
+                    row--;
+                    col--;
+                }
+
+                else if (lcs[row - 1, col] > lcs[row, col - 1])
+                {
+                    row--;
+                }
+
+                else
+                {
+                    col--;
+                }
+            }
+
+            sequence.Reverse();
+            Console.WriteLine(String.Join(" ", sequence));
+            Console.WriteLine(lcs[array1.Length, array2.Length]);
+        }
+    }
+}

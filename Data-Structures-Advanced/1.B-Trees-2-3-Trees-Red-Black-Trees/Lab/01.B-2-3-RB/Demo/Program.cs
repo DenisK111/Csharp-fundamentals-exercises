ba8af6b92acd163ba91f8b9b60ab436d821fe9fc
DenisK111 <denislavkraev@gmail.com>
2022-12-26 18:06:53 +0200

    added B-Trees-2-3-Tress-Red-Black Lab

---
+++ Data-Structures-Advanced/1.B-Trees-2-3-Trees-Red-Black-Trees/Lab/01.B-2-3-RB/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _01.Two_Three;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new TwoThreeTree<string>();
+            String[] arr = { "F", "C", "G", "A", "B", "D", "E", "K", "I", "G", "H", "J", "K" };
+            for (int i = 0; i < 13; i++)
+            {
+                tree.Insert(arr[i]);
+            }
+
+            ;
+
+            Console.WriteLine(tree);
+
+        }
+    }
+}
